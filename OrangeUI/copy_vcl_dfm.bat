a61f0581683f522c1471d6ca47392a7358e5e356
DelphiTeacher <ggggcexx@163.com>
2026-03-24 10:12:30 +0800

    。。

---
+++ OrangeUI/copy_vcl_dfm.bat
@@ -0,0 +1,119 @@
+
+
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_3\Win32\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_3\Win64\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_3\Win32\Release\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_3\Win64\Release\
+
+
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_4\Win32\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_4\Win64\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_4\Win32\Release\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D10_4\Win64\Release\
+
+
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D11\Win32\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D11\Win64\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D11\Win32\Release\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D11\Win64\Release\
+
+
+
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D12\Win32\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D12\Win64\Debug\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D12\Win32\Release\
+copy OrangeUIControl\Source\VCL\*.dfm OrangeUIDesign\DCU\VCL\D12\Win64\Release\
+
+
+
+
+
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_3\Win32\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_3\Win64\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_3\Win32\Release\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_3\Win64\Release\
+
+
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_4\`Win32\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_4\Win64\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_4\Win32\Release\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D10_4\Win64\Release\
+
+
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D11\Win32\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D11\Win64\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D11\Win32\Release\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D11\Win64\Release\
+
+
+
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D12\Win32\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D12\Win64\Debug\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D12\Win32\Release\
+copy OrangeUIControl\Source\FMX\*.fmx OrangeUIDesign\DCU\FMX\D12\Win64\Release\
+
+
+
+
+
+
+
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_3\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_3\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_3\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_3\Win64\Release\
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_4\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_4\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_4\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D10_4\Win64\Release\
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D11\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D11\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D11\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D11\Win64\Release\
+
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D12\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D12\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D12\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\VCL\D12\Win64\Release\
+
+
+
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_3\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_3\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_3\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_3\Win64\Release\
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_4\`Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_4\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_4\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D10_4\Win64\Release\
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D11\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D11\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D11\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D11\Win64\Release\
+
+
+
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D12\Win32\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D12\Win64\Debug\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D12\Win32\Release\
+copy OrangeUIControl\province_data.RES OrangeUIDesign\DCU\FMX\D12\Win64\Release\
+
+
+
+PAUSE
+
+
